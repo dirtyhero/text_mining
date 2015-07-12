63be3a6ac4fa037cba63c481a87d32b22b0be75d
高橋和樹 <takahashi@temona.co.jp>
2015-07-12 12:38:00 +0900

    fix login role fix link to home

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,8 @@
 class Admin::ApplicationController < ActionController::Base
   layout 'admin/layouts/application'
+  before_filter :go_to_home
+  private
+  def go_to_home
+    redirect_to controller: 'home', action: 'index' unless user_signed_in?
+  end
 end
