83b9f5bdd10ab389fcf46aa1611eeb8a9df42bbe
高橋和樹 <takahashi@temona.co.jp>
2015-07-16 10:30:07 +0900

    [add] チャートデータの配列を作るmethodを作成

--- app/controllers/admin/word_analysis_controller.rb
+++ app/controllers/admin/word_analysis_controller.rb
@@ -8,48 +8,14 @@ class Admin::WordAnalysisController < Admin::ApplicationController
     @three = []
     @four = []
     @five = []
-    # elemets = [@one,@two,@three,@four,@five]
-    # # [@one,@two,@three,@four,@five].map! do |chart_element|
-    # #   create_chart_data(top5[index])
-    # # end
-    # p top5
-
-    # top5.each_with_index do |word,i|
-    #   p "sldkjflsdjflksjdlfkjslkdfjlsjf;alkjd;fja;lj"
-    #   elemets[i] << create_chart_data(word)
-    # end
-    # p "fuckoffffffff"
-    # p elements
-    
+    elemets = [@one,@two,@three,@four,@five]
+    top5 = WordAnalysis.get_chart_top5(Date.today)
+    WordAnalysis.get_chart_top5(Date.today).each_with_index do |word,i|
+      elemets[i] << WordAnalysis.word_from_chart_date(word)
+    end
     @chart_data = [['2014-04-01', 60], ['2014-04-02', 65], ['2014-04-03', 64]]
     @word_ranking = ['注文検索','シナリオ','重要項目設定','ヘルプセンター','レコメンド']
   end
   private
 
-  def get_chart_top5
-    # todo Date.todayに変更
-    return WordAnalysis.find_by_sql('select word,(count(id)) as counts from word_analyses WHERE answering_date = "2015-07-09" group by word order by counts desc limit 5')
-  end
-
-  def create_chart_data(word_analysis)
-    chart_data = []
-    [5,4,3,2,1].each do |num|
-      date = Date.today - num
-      sql = word_from_create_sql(word_analysis,date)
-      p "chart_countchart_countchart_countchart_countchart_countchart_count"
-      chart_count = WordAnalysis.find_by_sql(sql)
-
-
-      chart_data << [chart_count,date,chart_count.counts]
-    end
-    return chart_data
-  end
-
-  def word_from_create_sql(word,day)
-    # 日付と単語を渡してその日付とカウントを返す
-    sql = "select (count(id)) as counts,answering_date as date from word_analyses WHERE word = '"
-    sql += word.word.to_s
-    sql += "' and answering_date > '2015-07-01'"
-    return sql
-  end
 end
--- app/models/word_analysis.rb
+++ app/models/word_analysis.rb
@@ -1,49 +1,45 @@
 class WordAnalysis < ActiveRecord::Base
- require 'csv'
- require 'natto'
- require 'pp'
- require 'kconv'
+  require 'csv'
+  require 'natto'
+  require 'pp'
+  require 'kconv'
+  # Admin::MenuControllerのcsv_text,u_id変数を持ってきます。インスタンス変数を直で突っ込むのがRailsのどっかのversionよりできないので、
+  # 下記のような処理を入れています。
+  csv_text = @csv_text
+  u_id = @u_id
 
 
   class << self
-
-    # def test(filepath)
-    #     p "aaaaa"
-
-    # end
-
-
-
-#Admin::MenuControllerのcsv_text,u_id変数を持ってきます。インスタンス変数を直で突っ込むのがRailsのどっかのversionよりできないので、
-#下記のような処理を入れています。
-csv_text = @csv_text
-u_id = @u_id
-
-#fileアップロードmethod
-def import(csv_text,u_id)
-
-
- CSV.parse(csv_text) do |data|
-  #Natto::MeCabクラスのオブジェクトを作成します。
-  nm = Natto::MeCab.new
-    #Natto::MeCabクラスのparseメソッドを呼び出し、形態素分析をします。
-    nm.parse(data[1]) do |n|
-    	#形態素分析を行った結果を格納する用のオブジェクトを生成。
-      wa = WordAnalysis.new
-      #surfaceで形態素分解した要素一個一個を。featureは左記要素の品詞(名詞とか形容詞とか)を返します。
-      #一列目は日付を想定しています。
-      #deletedは0を渡すようにします。
-      wa.answering_date=data[0]
-      wa.word = n.surface
-      wa.part = n.feature.split(",")[0]
-      wa.deleted = 0
-      wa.user_id = u_id
-      wa.save!
+    def import(csv_text,u_id) # fileアップロードmethod
+      CSV.parse(csv_text) do |data|
+      # Natto::MeCabクラスのオブジェクトを作成します。
+      nm = Natto::MeCab.new
+        # Natto::MeCabクラスのparseメソッドを呼び出し、形態素分析をします。
+        nm.parse(data[1]) do |n|
+          # 形態素分析を行った結果を格納する用のオブジェクトを生成。
+          wa = WordAnalysis.new
+          # surfaceで形態素分解した要素一個一個を。featureは左記要素の品詞(名詞とか形容詞とか)を返します。
+          # 一列目は日付を想定しています。
+          # deletedは0を渡すようにします。
+          wa.answering_date=data[0]
+          wa.word = n.surface
+          wa.part = n.feature.split(",")[0]
+          wa.deleted = 0
+          wa.user_id = u_id
+          wa.save!
+        end
+      end
     end
-   end
-  end
- end
+    def get_chart_top5(date=nil)
+      self.limit(5).group('word').order('count_id desc').count('id')
+    end
+    def word_from_chart_date(word)
+      self.where('word >= ?', word).group('answering_date').order('count_id desc').count('id').to_a
+    end
 
-   end
+  end # self end
+
+
+end
 
 
