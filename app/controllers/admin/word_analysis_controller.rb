9d4b01866db5dee934722e3b97a8b2d929ee8aca
高橋和樹 <takahashi@temona.co.jp>
2015-07-16 19:13:06 +0900

    [fix] order by 条件を修正
    変数名を修正

--- app/controllers/admin/word_analysis_controller.rb
+++ app/controllers/admin/word_analysis_controller.rb
@@ -4,10 +4,10 @@ class Admin::WordAnalysisController < Admin::ApplicationController
   def index
     @one = @two = @three = @four = @five = []
     elemets = [@one,@two,@three,@four,@five]
-    top5 = WordAnalysis.get_chart_top5(Date.today)
+    @top5 = WordAnalysis.get_chart_top5(Date.today)
     WordAnalysis.get_chart_top5(Date.today).each_with_index do |word,i|
-      if wa = WordAnalysis.word_from_chart_date(word)
-        elemets[i] << wa
+      if word_analiysis = WordAnalysis.word_from_chart_date(word)
+        elemets[i] << word_analiysis
       end
     end
     @chart_data = [['2014-04-01', 60], ['2014-04-02', 65], ['2014-04-03', 64]]
