51331e253f399d76109432ab52dd2017ad70579b
高橋和樹 <takahashi@temona.co.jp>
2015-07-16 18:39:21 +0900

    [add] チャート配列の完成(残り、viewのみ)

--- app/controllers/admin/word_analysis_controller.rb
+++ app/controllers/admin/word_analysis_controller.rb
@@ -2,20 +2,16 @@ class Admin::WordAnalysisController < Admin::ApplicationController
   require 'chartkick'
   require 'Date'
   def index
-    top5 = get_chart_top5
-    @one = []
-    @two = []
-    @three = []
-    @four = []
-    @five = []
+    @one = @two = @three = @four = @five = []
     elemets = [@one,@two,@three,@four,@five]
     top5 = WordAnalysis.get_chart_top5(Date.today)
     WordAnalysis.get_chart_top5(Date.today).each_with_index do |word,i|
-      elemets[i] << WordAnalysis.word_from_chart_date(word)
+      if wa = WordAnalysis.word_from_chart_date(word)
+        elemets[i] << wa
+      end
     end
     @chart_data = [['2014-04-01', 60], ['2014-04-02', 65], ['2014-04-03', 64]]
     @word_ranking = ['注文検索','シナリオ','重要項目設定','ヘルプセンター','レコメンド']
   end
   private
-
 end
